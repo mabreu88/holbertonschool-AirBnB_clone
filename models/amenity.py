d92efcc16d1fcba504dd20ce50d6f5eeb809457c
MicaViera <vieranoelia98@gmail.com>
2023-10-24 16:49:47 -0700

    Amenity class that inherits from BaseModel class.

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel class."""
+
+    def __init__(self, *args, **kwargs):
+        """Method that initializes the class."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
