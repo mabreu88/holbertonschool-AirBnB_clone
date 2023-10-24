8c328ccdb228b1ad09b802a40c8e671f7016c1b8
MicaViera <vieranoelia98@gmail.com>
2023-10-24 16:51:51 -0700

    State class that inherits from BaseModel class.

--- models/state.py
+++ models/state.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""State Class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel class."""
+
+    def __init__(self, *args, **kwargs):
+        """Method that initializes the class."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
