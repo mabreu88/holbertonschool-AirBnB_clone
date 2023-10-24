2739fb9163b0b6b10ad318e9d190f3bd37762cc9
MicaViera <vieranoelia98@gmail.com>
2023-10-24 16:50:19 -0700

    City class that inherits from BaseModel class.

--- models/city.py
+++ models/city.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Class city."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that inherits from BaseModel class."""
+
+    def __init__(self, *args, **kwargs):
+        """Method that initializes the class."""
+        super().__init__(*args, **kwargs)
+        self.state_id = ""
+        self.name = ""
