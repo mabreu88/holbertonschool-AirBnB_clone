2d6212515ccb7458b8a274694cebbe6cc3dc00ae
mabreu88 <martymalkav7@gmail.com>
2023-10-24 16:58:10 -0700

    User class

--- models/user.py
+++ models/user.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+from models.base_model import BaseModel
+""" Define the User class """
+
+
+class user(BaseModel):
+    """ User atributes """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
