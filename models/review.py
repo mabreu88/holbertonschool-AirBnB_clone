b9116250bb9fa36225db34fece7d90614ef722c3
MicaViera <vieranoelia98@gmail.com>
2023-10-24 16:51:26 -0700

    Review class that inherits from BaseModel class.

--- models/review.py
+++ models/review.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Review Class."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class that inherits from BaseModel class."""
+
+    def __init__(self, *args, **kwargs):
+        """Method that initializes the class."""
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
