2c51cdfbdb5b7eebc2cfbab13734b793a6303d99
MicaViera <vieranoelia98@gmail.com>
2023-10-26 13:51:26 -0700

    The console main program.

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Airnbnb project console."""
 import cmd
 import re
 
@@ -13,6 +14,7 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
+    """Class that manages the console."""
 
     prompt = '(hbnb) '
 
@@ -26,16 +28,54 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
+    def do_create(self, arg):
+        """Method that creates a new instance of BaseModel."""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            instance = BaseModel
+            json.dump(instance)
+            print(f"{instance.id}")
+
+    def do_show(self, arg):
+        """Method that prints the string representation of an instance."""
+        _input = arg.split()
+        if not _input:
+            print("** class name missing **")
+        elif _input[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(_input) < 2:
+            print("** instance id missing **")
+
+    def do_destroy(self, arg):
+        """Method that deletes instances based on the class name and id"""
+        _input = arg.split()
+        if not _input:
+            print("** class name missing **")
+        elif _input[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(_input) < 2:
+            print("** instance id missing **")
+
+    def do_all(self):
+        """Method that prints all string representation of all instances"""
+
     def emptyline(self):
+        """Method that creates an emppty line."""
         pass
 
     def do_quit(self, arg):
+        """Method that quits the program."""
         return True
 
     def do_EOF(self, arg):
+        """Method for the end of file."""
         return True
 
     def help_quit(self):
+        """Method that manages the help for the commands.s"""
         print("Prendiste fuego todo, sali cagando")
 
 
