375b5f8e83ded5b9021f44dec95b856d832aab0d
MicaViera <vieranoelia98@gmail.com>
2023-10-27 12:08:59 -0700

    Update class review.

--- models/review.py
+++ models/review.py
@@ -6,6 +6,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Review class that inherits from BaseModel class."""
 
-    self.place_id = ""
-    self.user_id = ""
-    self.text = ""
+    place_id = ""
+    user_id = ""
+    text = ""
