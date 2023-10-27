42243c762df6129cd157bc0dd0a066e324b29864
MicaViera <vieranoelia98@gmail.com>
2023-10-27 10:47:07 -0700

    Update class review.

--- models/review.py
+++ models/review.py
@@ -6,9 +6,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Review class that inherits from BaseModel class."""
 
-    def __init__(self, *args, **kwargs):
-        """Method that initializes the class."""
-        super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
+    self.place_id = ""
+    self.user_id = ""
+    self.text = ""
