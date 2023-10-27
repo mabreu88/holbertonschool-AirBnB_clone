84cfb97deeea68dcc4b4e8fa80ad6ce86613eb10
MicaViera <vieranoelia98@gmail.com>
2023-10-27 10:47:31 -0700

    Update class state.

--- models/state.py
+++ models/state.py
@@ -6,7 +6,4 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """State class that inherits from BaseModel class."""
 
-    def __init__(self, *args, **kwargs):
-        """Method that initializes the class."""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+    self.name = ""
