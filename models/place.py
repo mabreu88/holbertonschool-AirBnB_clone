9f22fcc3d757a9eb71304bbd1a37fae31b968dac
MicaViera <vieranoelia98@gmail.com>
2023-10-27 12:08:33 -0700

    Update class place.

--- models/place.py
+++ models/place.py
@@ -6,14 +6,14 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """Place class that inherits from BaseModel class."""
 
-    self.city_id = ""
-    self.user_id = ""
-    self.name = ""
-    self.description = ""
-    self.number_rooms = 0
-    self.number_bathrooms = 0
-    self.max_guest = 0
-    self.price_by_night = 0
-    self.latitude = 0.0
-    self.longitud = 0.0
-    self.amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitud = 0.0
+    amenity_ids = []
