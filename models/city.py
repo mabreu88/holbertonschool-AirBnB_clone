5bb3918d4ad99ecfda4c5971a231a496b8279fd9
MicaViera <vieranoelia98@gmail.com>
2023-10-27 12:08:15 -0700

    Update class city.

--- models/city.py
+++ models/city.py
@@ -6,5 +6,5 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """City class that inherits from BaseModel class."""
 
-    self.state_id = ""
-    self.name = ""
+    state_id = ""
+    name = ""
