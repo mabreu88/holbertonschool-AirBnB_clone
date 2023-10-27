ec4e4ce7fdd02720a74d8afd787f722f5538db30
MicaViera <vieranoelia98@gmail.com>
2023-10-27 10:46:24 -0700

    Update class city.

--- models/city.py
+++ models/city.py
@@ -6,8 +6,5 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """City class that inherits from BaseModel class."""
 
-    def __init__(self, *args, **kwargs):
-        """Method that initializes the class."""
-        super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
+    self.state_id = ""
+    self.name = ""
