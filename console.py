5d610de754a4dd4e3057d24ce73d5dbb7ed4381a
mabreu88 <martymalkav7@gmail.com>
2023-10-28 09:39:26 -0700

    restore default

--- console.py
+++ console.py
@@ -31,6 +31,28 @@ class HBNBCommand(cmd.Cmd):
         "Review": Review
     }
 
+    def default(self, arg):
+        """Default Metod that matches arguments."""
+
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "create": self.do_create,
+            "update": self.do_update,
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
     def emptyline(self):
         """Method that creates an emppty line."""
         pass
